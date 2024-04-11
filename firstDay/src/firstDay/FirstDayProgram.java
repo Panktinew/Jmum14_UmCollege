b5ececd67854829685ab0a5a969876e61adead4c
Panktinew <130337976+Panktinew@users.noreply.github.com>
2024-04-11 19:46:55 +0530

    Add files via upload

---
+++ firstDay/src/firstDay/FirstDayProgram.java
@@ -0,0 +1,21 @@
+package firstDay;
+
+public class FirstDayProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("WElcome to the TNS India Foundation");
+		
+		/*  write sysout and press ctrl + space key togather */
+		
+		  int no = 34;  // it is a single character
+		  System.out.println("Here is my answer"+ no );
+
+		 float result =  (float) no/3;
+		 System.out.println("division =" + result);
+		  
+	}
+
+}
